a946083bf7c2797470820440cba49f7f5d4302c4
sankar-aws <sankaranaws@gmail.com>
2025-11-04 17:07:00 +0530

    Update InstagramApp.java

--- InstagramApp.java
+++ InstagramApp.java
@@ -11,7 +11,7 @@ public class InstagramApp {
         Scanner sc = new Scanner(System.in);
         String choice;
 
-        System.out.println("=== Welcome to instagram CLI ===");
+        System.out.println("=== Welcome to instagram GUI ===");
 
         do {
             System.out.println("\n1. Post a photo");
